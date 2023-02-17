36406cff6abb24138e11b2540e68771b364d0107
Fransedo Ravelino <fransedo.ravelino@BLUEBIRDGROUP.COM>
2023-02-17 11:09:21 +0700

    adjusted start playback endpoint

--- controllers/playback.go
+++ controllers/playback.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"log"
+	"net/http"
 	"os"
 
 	helper "github.com/fransedor/golang-spotify/helpers"
@@ -10,19 +11,24 @@ import (
 )
 
 func StartPlayback(c *gin.Context) {
+	client := &http.Client{}
 	authHeader := c.Request.Header.Get("Authorization")
 	spotifyURL := os.Getenv("SPOTIFY_URL")
 	req, err := helper.CreateHTTPRequestWithHeader("PUT", spotifyURL+"/me/player/play", nil, authHeader[7:])
 	if err != nil {
 		log.Fatal(err)
 	}
-	var response map[string]string
-	var status string
-	status, err = helper.GetHTTPResponse(req, &response)
+	response, err := client.Do(req)
 	if err != nil {
 		log.Fatal(err)
 	}
-	c.IndentedJSON(200, status)
+	defer response.Body.Close()
+
+	responseObj := make(map[string]any)
+	responseObj["status"] = 200
+	responseObj["message"] = "started playback"
+
+	c.IndentedJSON(200, responseObj)
 }
 
 func GetCurrentlyPlaying(c *gin.Context) {
